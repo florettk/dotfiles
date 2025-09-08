6c54c142df7e1be067cafaf5d31b4c46c564474e
KUZHEL Ivan <ruakuir@n-spb-mackuir-2.raiffeisen.ru>
2025-09-08 16:02:48 +0300

    theme added

--- .wezterm.lua
+++ .wezterm.lua
@@ -14,7 +14,7 @@ config.colors = {
 	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
 }
 
-config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 19
 
 config.enable_tab_bar = false
